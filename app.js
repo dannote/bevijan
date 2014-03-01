769b1a0ae7a251d0096845d5fb85b5cc2cb08c46
Danila Poyarkov <dannotemail@gmail.com>
2014-03-01 23:16:01 +0400

    Better way

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var url = require('url');
 http.createServer(function(request, response) {
 	try {
 		var code = url.parse(request.url, true).query.code;
-		response.end(code ? JSON.stringify(eval(code)) : 'All right');
+		response.end(code ? '' + eval(code) : 'All right');
 	} catch (error) {
 		response.end(error.stack);
 	}
